49d9af9fdf934b6966dd00e58a45c8ac40bb96ea
Toby McCrae <zhac161@live.rhul.ac.uk>
2020-06-09 00:03:57 +0100

    Added code to update the flag_color collumn in the database

--- app.py
+++ app.py
@@ -28,10 +28,12 @@ def create_buggy():
     msg=""
     try:
       qty_wheels = request.form['qty_wheels']
-      msg = f"qty_wheels={qty_wheels}" 
+      flag_color = request.form['flag_color']
+      msg = f"qty_wheels={qty_wheels}"
+      msg = f"flag_color={flag_color}"
       with sql.connect(DATABASE_FILE) as con:
         cur = con.cursor()
-        cur.execute("UPDATE buggies set qty_wheels=? WHERE id=?", (qty_wheels, DEFAULT_BUGGY_ID))
+        cur.execute("UPDATE buggies set qty_wheels=?, flag_color=? WHERE id=?", (qty_wheels, flag_color, DEFAULT_BUGGY_ID))
         con.commit()
         msg = "Record successfully saved"
     except:
